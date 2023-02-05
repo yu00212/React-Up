c3f59a7e7701657ce9bb4851ca7c0023fc1416f8
ゆう <yu2000212@icloud.com>
2023-02-05 11:10:04 +0000

    レンダリングの最適化（Callback）

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -8,9 +8,10 @@ export default function App() {
 
   const onChangeText = (e) => setText(e.target.value);
 
-  const onClickOpen = () => {
-    setOpen(!open);
-  }
+  const onClickOpen = () => setOpen(!open);
+
+  // useCallbackで関数のmemo化
+  const onClickClose = () => useCallback(setOpen(false), [setOpen]);
 
   return (
     <div className="App">
@@ -18,7 +19,7 @@ export default function App() {
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open}/>
+      <ChildArea open={open} onClickClose={onClickClose}/>
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -14,7 +14,7 @@ const style = {
 // アロー関数全体をmemo()で囲むことで、propsが変更されない限りこのコンポーネントは再レンダリングされなくなる
 
 export const ChildArea = memo((props) => {
-    const { open } = props;
+    const { open, onClickClose } = props;
     console.log("ChildAreaがレンダリングされた!!");
 
     const data = [...Array(2000).keys()];
@@ -27,6 +27,7 @@ export const ChildArea = memo((props) => {
             { open ? (
                 <div style={style}>
                     <p>子コンポーネント</p>
+                    <button onClick={onClickClose}>閉じる</button>
                 </div>
             ) : null}
         </>
